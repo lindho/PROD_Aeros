ebbbc618a6cab080310f706bf3c0af1e4518ec31
lindho <adam.lindstrm@gmail.com>
2017-09-12 03:02:51 +0200

    Added player and audio
    Added a player with two scripts, will have to be revised to work on a top down environment. Folders with music and sound effects added.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour {
+	Vector3 velocity;
+	Rigidbody myBody;
+
+	void Start () {
+		myBody = GetComponent<Rigidbody>();
+	}
+	
+	public void Move(Vector3 _velocity){
+		velocity = _velocity;
+	}
+
+	public void FixedUpdate(){
+		myBody.MovePosition (myBody.position + velocity * Time.fixedDeltaTime);
+	}
+}

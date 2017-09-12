271621272af13f7841bed9638f55921a79c8222b
lindho <adam.lindstrm@gmail.com>
2017-09-12 19:53:59 +0200

    Player inputs working
    Smoothing for player inputs is to be implemented next. A camerafollow script was created but failed because of different reasons

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (PlayerController))]
+[RequireComponent(typeof (Collider))]
+public class Player : MonoBehaviour {
+	
+	public float moveSpeed = 5f;
+	PlayerController controller;
+	Collider collider;
+	[HideInInspector]
+	public Vector2 moveInput;
+
+
+	void Start () {
+		controller = GetComponent <PlayerController>();
+		collider = GetComponent <Collider> ();
+	}
+
+	public void Update(){
+		Vector2 moveInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		Vector2 moveVelocity = moveInput.normalized * moveSpeed;
+		controller.Move (moveVelocity);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,14 @@ using UnityEngine;
 
 
 public class PlayerController : MonoBehaviour {
-	public float moveSpeed = 5;
 
-	void Start () {
+	Vector3 velocity;
+
+	public void Move(Vector3 _velocity){
+		velocity = _velocity;
 	}
-	
-//	public void Move(Vector3 _velocity){
-//		velocity = _velocity;
-//	}
 
 	public void FixedUpdate(){
-		if (Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw ("Horizontal") < -0.5f) {
-		
-			transform.Translate = (new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.fixedDeltaTime, 0f, 0f));
-		
-		}
-
-		if (Input.GetAxisRaw ("Vertical") > 0.5f || Input.GetAxisRaw ("Vertical") < -0.5f) {
-
-			transform.Translate = (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.fixedDeltaTime, 0f));
-
-		}
+		transform.position+=transform.forward + velocity *Time.fixedDeltaTime;
 	}
 }

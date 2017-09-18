6751b5438e54942cce82fc5ca69657b37e905647
lindho <adam.lindstrm@gmail.com>
2017-09-18 12:17:59 +0200

    fixed enemy returnpath
    need to write a A* pathfinding script to prevent enemy from going through obstacles

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,42 +4,56 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-	Vector2 offset;
+	float attackDistanceThreshold;
+	float timeBetweenAttacks = 1;
+
+	float nextAttackTime;
+
 	bool isAggro;
 	public Transform target;
-	float enemySpeed = 3f;
-	private Vector2 startPosition;
+	float moveSpeed = 3f;
+	public GameObject startPosition;
 
 	void Start () {
 		isAggro = false;
-		startPosition = transform.position;
 	}
 
 	void Update () {
 		Aggro ();
+
+		if (Time.time > nextAttackTime) {
+			float sqrDstToTarget = (target.position - transform.position).sqrMagnitude;
+			if (sqrDstToTarget < Mathf.Pow (attackDistanceThreshold, 2)) {
+				nextAttackTime = Time.time + timeBetweenAttacks;
+				StartCoroutine (Attack ());
+			}
+		}
+	}
+
+	IEnumerator Attack(){
+		Vector2 originalPosition = transform.position;
+		Vector3 attackPosition = target.position;
+
+		yield return null;
 	}
 
 	void Harm() {
 		
 	}
 
-	void Attack(){
-		
-	}
-
 	void Aggro (){
-		float attack = enemySpeed * Time.deltaTime;
-		if (transform.position.magnitude <= 10) {
-			isAggro = true;
+		isAggro = true;
+		float attack = moveSpeed * Time.deltaTime;
+		if ((transform.position - target.transform.position).magnitude < 10f && isAggro) {
 			transform.position = Vector3.MoveTowards (transform.position, target.position/* - offset*/, attack);
-		} else if (transform.position.magnitude >= 10) {
+		} else if ((transform.position - target.transform.position).magnitude > 10f) {
 			ReturnToStartPoint ();
 		}
 	}
 
 	void ReturnToStartPoint(){
 		isAggro = false;
-		float retreat = (enemySpeed) * Time.deltaTime;
-		transform.position = Vector3.MoveTowards (transform.position, startPosition, retreat);
+		float retreat = (moveSpeed*1.5f) * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, startPosition.transform.position, retreat);
 	}
 }
